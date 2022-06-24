0ed4730c6f3e4d1f011e5109f4467660844f0175
Beckham 45 <o.becks45@gmail.com>
2022-06-24 12:22:10 +0300

    Updates

---
+++ pages/analytics/statisticalanalytics.js
@@ -0,0 +1,28 @@
+import { useRouter } from "next/router";
+import React from "react";
+import { useSelector } from "react-redux";
+import Channelengagementgraph from "../../components/analytics/channelengagementgraph";
+import Userengagementtrend from "../../components/analytics/userengagementtrend";
+import { selectUser } from "../../redux/authentication/authslice";
+function Statisticalanalytics() {
+  const router = useRouter();
+
+  const user = useSelector(selectUser);
+  const token = user.user.token;
+
+  if (typeof window !== "undefined" && user == null)
+    router.push("/authentication/signin");
+
+  return (
+    <div>
+      <div>
+        <Userengagementtrend />
+      </div>
+      <div>
+        <Channelengagementgraph />
+      </div>
+    </div>
+  );
+}
+
+export default Statisticalanalytics;
