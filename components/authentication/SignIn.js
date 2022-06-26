b89b8c390fc122f371ebf0c9a2fd0e20d077ce44
Beckham 45 <o.becks45@gmail.com>
2022-06-26 22:32:07 +0300

    Updates

--- components/authentication/SignIn.js
+++ components/authentication/SignIn.js
@@ -34,7 +34,7 @@ const Login = (props) => {
 
     await axios({
       method: "post",
-      url: "http://proximaawsserver.xyz:8000/api/usersauth/userlogin/",
+      url: "https://proximaawsserver.xyz:8000/api/usersauth/userlogin/",
       data: formField,
     })
       .then((response) => {
