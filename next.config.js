0a20bd3ee7215beb6063a35d843f19d57524abec
Beckham Ochieng <o.becks45@gmail.com>
2023-06-08 12:24:31 +0300

    updates

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 const path = require('path');
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // reactStrictMode: true,
-  // distDir: 'build',
-  output: 'standalone',
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias['@'] = path.resolve(__dirname);
-    }
+module.exports = {
+  webpack: (config) => {
+    config.resolve.alias['@'] = path.resolve(__dirname, '.');
+
     return config;
   },
 };
-
-module.exports = nextConfig;
